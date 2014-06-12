62e6aeb779676f7d66d30eae085ceadfd5a3b26b
Stuart Young <stuartpyoung@gmail.com>
2014-06-12 15:32:29 -0700

    [0.8.0-beta.1+build.27] Fixed GT Tools install missing Makefile create step

--- agua/gt/gt.pm
+++ agua/gt/gt.pm
@@ -7,9 +7,15 @@ method doInstall ($installdir, $version) {
     $self->logDebug("installdir", $installdir);
     $version = $self->version() if not defined $version;
     
-        return 0 if not $self->gitInstall($installdir, $version);
-    return 0 if not $self->makeInstall($installdir, $version);
+    return 0 if not $self->gitInstall($installdir, $version);
 
+    #### GENERATE MAKEFILE
+    $self->changeDir("$installdir/$version");
+	my ($out, $err) = $self->runCommand("perl Makefile.PL");
+	$self->logDebug("out", $out);
+	$self->logDebug("err", $err);
+
+    return 0 if not $self->perlmakeInstall($installdir, $version);
 
     return $version;
 }
