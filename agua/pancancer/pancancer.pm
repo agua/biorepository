4a97b2a324fb3326a1e9d03a61aad4c29bc64c60
Stuart Young <stuartpyoung@gmail.com>
2014-07-02 17:25:49 -0700

    [0.8.0-beta.1+build.35] Changed pancancer.pm to return 1

--- agua/pancancer/pancancer.pm
+++ agua/pancancer/pancancer.pm
@@ -14,15 +14,19 @@ method doInstall ($installdir, $version) {
 }
 
 method loadWorkflow ($installdir, $version) {
-# Delete old project entry if exists
-$self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl ./PanCancer.proj delete --username syoung");
+    # Delete old project entry if exists
+    $self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl ./PanCancer.proj delete --username syoung");
+    
+    # Add project file to database
+    $self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl ./PanCancer.proj save --username syoung");
+    
+    # Add workflows to project (and save to database)
+    $self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl PanCancer.proj saveWorkflow --wkfile 1-Download.work --username syoung");
+    $self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl PanCancer.proj saveWorkflow --wkfile 2-Split.work --username syoung");
+    $self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl PanCancer.proj saveWorkflow --wkfile 3-Align.work --username syoung");
 
-# Add project file to database
-$self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl ./PanCancer.proj save --username syoung");
-
-# Add workflows to project (and save to database)
-$self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl PanCancer.proj saveWorkflow --wkfile 1-Download.work --username syoung");
-$self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl PanCancer.proj saveWorkflow --wkfile 2-Split.work --username syoung");
-$self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl PanCancer.proj saveWorkflow --wkfile 3-Align.work --username syoung");
-
+    return 1;
 }
+
+
+1;
