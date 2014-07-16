434ec4edf22504ea4caf50928d007de9c363a757
Stuart Young <stuartpyoung@gmail.com>
2014-07-15 17:19:46 -0700

    [0.8.0-beta.1+build.39] Added 'aspera' package

---
+++ agua/aspera/aspera.pm
@@ -0,0 +1,41 @@
+package aspera;
+use Moose::Role;
+use Method::Signatures::Simple;
+
+method doInstall ($installdir, $version) {
+    $self->logDebug("version", $version);
+    $self->logDebug("installdir", $installdir);
+    $version = $self->version() if not defined $version;
+    
+    #return 0 if not $self->zipInstall($installdir, $version);
+    #
+    #return 0 if not $self->scriptInstall($installdir, $version);
+
+    $self->confirmInstall($installdir, $version);
+    
+    return $version;
+}
+
+method scriptInstall ($installdir, $version) {
+    $self->logDebug("installdir", $installdir);
+    $self->logDebug("version", $version);
+    
+    my ($script)    =   $self->opsinfo()->url()   =~  /^.+?\/([^\/]+)\.tar\.gz/;
+    $self->logDebug("script", $script);
+
+    my $command     =   "$installdir/$script";
+    $self->runCommand($command);
+    
+    my $homedir     =   $ENV{'HOME'};
+    $self->logDebug("homedir", $homedir);
+    
+    my $sourcedir  =   "$homedir/.aspera/connect";
+    
+    $command        =   "cp -r $sourcedir   $installdir/$version";
+    $self->runCommand($command);
+    
+    return 1;
+}
+
+
+1;
