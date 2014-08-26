91d62b5eab908058a55605d66ee80b1b0d9f8a2f
Stuart Young <stuartpyoung@gmail.com>
2014-08-26 14:51:51 -0700

    [0.8.0-beta.1+build.54] Added support for ubuntu and osx in sra package

--- agua/sra/sra.pm
+++ agua/sra/sra.pm
@@ -8,9 +8,22 @@ method doInstall ($installdir, $version) {
     $version = $self->version() if not defined $version;
     
     my $arch    =   $self->getArch();
+    $self->logDebug("arch", $arch);
+    
     if ( $arch eq "centos" ) {
         $self->opsinfo()->url("http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/$version/sratoolkit.$version-centos_linux64.tar.gz");
         $self->opsinfo()->unzipped("sratoolkit.$version-centos_linux64");
+    }
+    elsif ( $arch eq "centos" ) {
+        $self->opsinfo()->url("http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/$version/sratoolkit.$version-ubuntu64.tar.gz");
+        $self->opsinfo()->unzipped("sratoolkit.$version-centos_linux64");
+    }
+    elsif ( $arch eq "osc" ) {
+        $self->opsinfo()->url("http://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/$version/sratoolkit.$version-mac64.tar.gz");
+        $self->opsinfo()->unzipped("sratoolkit.$version-centos_linux64");
+    }
+    else {
+        $self->logDebug("architecture not supported", $arch);
     }
 
     return 0 if not $self->zipInstall($installdir, $version);
