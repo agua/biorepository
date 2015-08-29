f26077bf8af6e3ed80cbb02f60b77b044a0bfd3c
Stuart Young <stuartpyoung@gmail.com>
2015-08-29 14:35:42 -0400

    [0.8.0-beta.1+build.61] Fixed commented sections in dnaseq.pm

--- agua/dnaseq/dnaseq.pm
+++ agua/dnaseq/dnaseq.pm
@@ -9,18 +9,22 @@ method doInstall ($installdir, $version) {
     
     #return 0 if not $self->gitInstall($installdir, $version);
 
+$self->version("0.0.1");
+    $version = $self->version();
+    $self->logDebug("AFTER gitInstall    version", $version);
+
 	#### LOAD APP FILES
     my $username    =   $self->username();
     my $package    =   $self->package();
-    my $appdir      =   "$installdir/latest/conf/apps";
+    my $appdir      =   "$installdir/$version/conf/app";
     my $format      =   "yaml";
 	$self->logDebug("Doing loadAppFiles");
-	$self->loadAppFiles($username, $package, $installdir, $appdir, $format);
+	#$self->loadAppFiles($username, $package, $installdir, $appdir, $format);
 	$self->logDebug("Completed loadAppFiles");
 
-    #return 0 if not $self->loadWorkflows($installdir, $version);
-    #
-    #return 0 if not $self->loadData($installdir, $version);
+    return 0 if not $self->loadWorkflows($installdir, $version);
+    
+    return 0 if not $self->loadData($installdir, $version);
     
     return $version;
 }
@@ -31,10 +35,8 @@ method loadWorkflows ($installdir, $version) {
     $self->logDebug("version", $version);
     
     my $basedir =   $self->conf()->getKey("agua", "INSTALLDIR");
-    my $project     =   $self->project();
     my $username    =   $self->username();
     $self->logDebug("basedir", $basedir);
-    $self->logDebug("project", $project);
     $self->logDebug("username", $username);
 
     #### DELETE EXISTING project ENTRY
@@ -46,13 +48,6 @@ method loadWorkflows ($installdir, $version) {
     #### ADD WORKFLOWS
     my $projects = [
         {
-            project =>  "FastQC",
-            workflows   =>  [
-                "FastQC"
-            ]
-        }
-        ,
-        {
             project =>  "Align",
             workflows   =>  [
                 "Bwa"
@@ -60,7 +55,7 @@ method loadWorkflows ($installdir, $version) {
         }
         ,
         {
-            project =>  "Process",
+            project =>  "DnaSeq",
             workflows   =>  [
                 "FixMates",
                 "FilterReads",
@@ -74,16 +69,59 @@ method loadWorkflows ($installdir, $version) {
                 "FreeBayes"
             ]
         }
-    ];
-    foreach my $project ( @$projects ) {
-        my $project     =   $project->{project};
-        my $workflows   =   $project->{workflows};
-        print "Loading workflows for project '$project'\n";
-
-        foreach my $workflow ( @$workflows ) {
-            $self->runCommand("cd $installdir/$version/conf && /agua/bin/cli/flow.pl CU.proj saveWorkflow --wkfile 1-LoadSamples.work --username $username");
+        ,
+        {
+            project =>  "FastQC",
+            workflows   =>  [
+                "FastQC"
+            ]
         }
-        print "Completed loading workflows for project '$project'\n\n";
+        ,
+        {
+            project =>  "Fork",
+            workflows   =>  [
+                "FastQcFork",
+                "HetHomFork",
+                "TsTvFork",
+                "RelatednessFork"
+            ]
+        }
+        ,
+        {
+            project =>  "QC",
+            workflows   =>  [
+                "DownloadQC",
+                "QCFork"
+            ]
+        }
+        ,
+        {
+            project =>  "Variant",
+            workflows   =>  [
+                "FreeBayes",
+                "Varscan"
+            ]
+        }
+    ];
+    
+    my $flow = "/a/bin/cli/flow";
+    foreach my $project ( @$projects ) {
+        my $projectname     =   $project->{project};
+        print "Loading workflows for project '$projectname'\n";
+        $self->logDebug("projectname", $projectname);
+
+        #### CREATE PROJECT
+        $self->runCommand("/a/bin/cli/flow deleteProject --project $projectname --username admin");
+        $self->runCommand("/a/bin/cli/flow addProject --project $projectname --username admin");
+
+        #### ADD WORKFLOWS TO PROJECT
+        my $workflows   =   $project->{workflows};
+        my $counter =   1;
+        foreach my $workflow ( @$workflows ) {
+            $self->runCommand("cd $installdir/$version/conf/work/$projectname && $flow addWorkflow --project $projectname --wkfile $workflow.work --username $username");
+            $counter++;
+        }
+        print "Completed loading workflows for project '$projectname'\n\n";
     }
 }
 
@@ -93,23 +131,21 @@ method loadData ($installdir, $version) {
     $self->logDebug("version", $version);
 
     my $basedir =   $self->conf()->getKey("agua", "INSTALLDIR");
-    my $project     =   $self->project();
     my $username    =   $self->username();
     $self->logDebug("basedir", $basedir);
-    $self->logDebug("project", $project);
     $self->logDebug("username", $username);
 
     my $table   =   "samples";
 
-    #### LOAD SAMPLES IDs
-    my $command     =   "$basedir/bin/sample/loadSamples.pl --username $username --project $project --workflow loadSamples --workflownumber 1 --file $installdir/$version/data/$table.tsv";
-    $self->logDebug("command", $command);
-    $self->runCommand($command);
-    
-    #### LOAD SAMPLES FILE NAMES AND SIZES
-    $command     =   "$basedir/bin/sample/loadTable.pl --table $table --tsvfile $installdir/$version/data/$table.tsv --sqlfile $installdir/$version/data/$table.sql";
-    $self->logDebug("command", $command);
-    $self->runCommand($command);
+    ##### LOAD SAMPLES IDs
+    #my $command     =   "$basedir/bin/sample/loadSamples.pl --username $username --project $project --workflow loadSamples --workflownumber 1 --file $installdir/$version/data/$table.tsv";
+    #$self->logDebug("command", $command);
+    #$self->runCommand($command);
+    #
+    ##### LOAD SAMPLES FILE NAMES AND SIZES
+    #$command     =   "$basedir/bin/sample/loadTable.pl --table $table --tsvfile $installdir/$version/data/$table.tsv --sqlfile $installdir/$version/data/$table.sql";
+    #$self->logDebug("command", $command);
+    #$self->runCommand($command);
 
     print "Completed loading samples\n\n";
 }
