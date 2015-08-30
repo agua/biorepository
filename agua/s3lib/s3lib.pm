9ca02c2c390d7586487cc7e8f398ead4b538d68a
Stuart Young <stuartpyoung@gmail.com>
2015-08-29 21:47:53 -0400

    [0.8.0-beta.1+build.65] Added method installDependencies to s3lib

--- agua/s3lib/s3lib.pm
+++ agua/s3lib/s3lib.pm
@@ -9,10 +9,44 @@ method doInstall ($installdir, $version) {
 
 	$version = $self->zipInstall($installdir, $version);
 
+	$self->installDependencies();
+	
 	$self->confirmInstall($installdir, $version);
 	
 	return $version;
 }
 
+method installDependencies {
+	my $arch = $self->getArch();
+	$self->logDebug("arch", $arch);
+
+	if ( $arch eq "ubuntu" ) {
+		my $commands = [ "apt-get install python-pip -y"];
+		foreach my $command ( @$commands ) {
+			my ($out, $err) = $self->runCommand($command);
+			$self->logDebug("out", $out);
+			$self->logDebug("err", $err);
+		}
+	}
+	elsif ( $arch eq "centos" ) {
+		my $commands = [ "yum install python-pip"];
+		foreach my $command ( @$commands ) {
+			my ($out, $err) = $self->runCommand($command);
+			$self->logDebug("out", $out);
+			$self->logDebug("err", $err);
+		}
+	}
+	else {
+		print "Architecture '$arch' is not supported yet. Please install pip manually\n";
+	}
+
+	my $commands = ["pip install boto", "pip install filechunkio"];
+	foreach my $command ( @$commands ) {
+		#$self->logDebug("command", $command);
+		my ($out, $err) = $self->runCommand($command);
+		$self->logDebug("out", $out);
+		$self->logDebug("err", $err);
+	}
+}
 
 1;
